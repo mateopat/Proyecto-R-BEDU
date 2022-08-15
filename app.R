716678f4f7678acd861d7eb5df52fb6e70d82d9e
JESUS MANUEL JIMENEZ CARDOZA <104805594+chucho003@users.noreply.github.com>
2022-08-15 00:45:55 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -20,15 +20,27 @@ ui <- dashboardPage(
                                
                                fluidRow(
                                   box(
-                                   title = "Histograma Basico",
+                                   title = "Estas son las graficas Barra",
                                    plotOutput("grafico1", height = 550)   
                                
                                  )
                                )
                                 ),# del TAB1
-                      tabPanel(name="dos", "Imagenes del Post work 3"),
+                      tabPanel(name="dos", "Imagenes del Post work 3",
+                               
+                               fluidRow(
+                                 titlePanel("Gráficas resultado del Postwork 3"), 
+                                 selectInput("x", "Selecciona la gráfica que desea ver",
+                                             choices = c("1", "2", "3")),
+                                 
+                                 imageOutput("image2")
+                                 #img(src = "POSTWORK3_1.png", 
+                                  #   height = 350, width = 550)
+                               )
+
+                               ),
                       tabPanel(name="tres", "Datos del fichero"),
-                      tabPanel(name="cuatro", "I,agenes de Factores")
+                      tabPanel(name="cuatro", "Imagenes de Factores")
                         )
                       )
                  )
@@ -39,19 +51,64 @@ server <- function(input, output) {
   output$grafico1 <- renderPlot({
     if (is.null(input$maximo) || is.null(input$color))
       return()
-    
     visual(input$maximo,input$color)
     
   })
 
   
+  output$image2 <- renderImage({
+    
+    if (is.null(input$x))
+      return(NULL)
+    
+    if (input$x == "1") {
+      return(list(
+        src = "www/1.png",
+        contentType = "image/png",
+        alt = "Grafica Resultado 1"
+      ))
+    } else if (input$x == "2") {
+      return(list(
+        src = "www/2.png",
+        filetype = "image/png",
+        alt = "Grafica Resultado 2"
+      ))
+    }
+    
+    else if (input$x == "3") {
+      return(list(
+        src = "www/3.png",
+        filetype = "image/png",
+        alt = "Grafica Resultado 3"
+      ))
+    }
   
-  output$uno <- renderText("Hola" ) #Titulo del main Panel
-  output$dos <- renderText("Hola" )  
-  output$tres <- renderDataTable( {iris},       #Data table
-                         options = list(aLengthMenu = c(10,20,50), iDisplayLength = 10) ) 
-  output$cuatro <- renderTable({ data.frame(iris)})   # Data Frame
   
+  
+  
+  })
+
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #output$uno <- renderText("Hola" ) #Titulo del main Panel
+  #output$dos <- renderText("Hola" )  
+  #output$tres <- renderDataTable( {iris},       #Data table
+  #                       options = list(aLengthMenu = c(10,20,50), iDisplayLength = 10) ) 
+#  output$cuatro <- renderTable({ data.frame(iris)})   # Data Frame
+  
+ # }
 
 shinyApp(ui, server)
